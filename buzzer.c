1c7fd954991e79f8ced8eab8b0639b8c0ecec862
myfreax <web@myfreax.com>
2024-01-30 18:32:37 +0800

    refactor: rename function name and move callback to static function

--- buzzer.c
+++ buzzer.c
@@ -7,55 +7,60 @@
 #include "timer.h"
 static const char* TAG = "BUZZER";
 
-esp_err_t buzzer_config(int buzzer_pin) {
+static void callback(void* arg) {
+  esp_timer_handle_t* timer = arg;
+  ESP_ERROR_CHECK(ledc_stop(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_7, 50));
+  esp_err_t err = esp_timer_delete(*timer);
+  if (err != ESP_OK) {
+    ESP_LOGE(TAG, "Timer Delete Failed: %s", esp_err_to_name(err));
+  }
+}
+
+esp_err_t buzzer_init(int buzzer_pin) {
   return pwm_config(LEDC_TIMER_3, LEDC_TIMER_10_BIT, 2700, LEDC_AUTO_CLK,
                     LEDC_CHANNEL_7, buzzer_pin);
 }
 
-esp_err_t buzzer_once(uint64_t time_us, uint8_t percentage) {
+esp_err_t buzzer_beep(uint64_t time_us, uint8_t percentage) {
   esp_err_t error = pwm_set_duty(LEDC_CHANNEL_7, LEDC_TIMER_10_BIT, percentage);
   if (error != ESP_OK) {
     return error;
   }
 
   static esp_timer_handle_t handle;
-  void callback(void* arg) {
-    esp_timer_handle_t* timer = arg;
-    ESP_ERROR_CHECK(ledc_stop(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_7, 50));
-    esp_err_t err = esp_timer_delete(*timer);
-    if (err != ESP_OK) {
-      ESP_LOGE(TAG, "Timer Delete Failed: %s", esp_err_to_name(err));
-    }
-  }
 
   ESP_ERROR_CHECK(
-      set_timeout("buzzer_once", callback, &handle, &handle, time_us));
+      set_timeout("buzzer_beep", callback, &handle, &handle, time_us));
   return ESP_OK;
 }
 
 static void alarm_task(void* arg) {
-  buzzer_params_t* alarm = arg;
+  buzzer_alarm_t* alarm = arg;
   while (1) {
     ESP_ERROR_CHECK(
         pwm_set_duty(LEDC_CHANNEL_7, LEDC_TIMER_10_BIT, alarm->percentage));
-    vTaskDelay(alarm->interval_time / portTICK_PERIOD_MS);
+    vTaskDelay(alarm->interval / portTICK_PERIOD_MS);
     ESP_ERROR_CHECK(pwm_stop(LEDC_CHANNEL_7));
-    vTaskDelay(alarm->interval_time / portTICK_PERIOD_MS);
+    vTaskDelay(alarm->interval / portTICK_PERIOD_MS);
   }
 }
 
-buzzer_params_t* buzzer_alarm(uint64_t interval_time_ms, uint8_t percentage) {
-  TaskHandle_t xtask_id;
-  buzzer_params_t* alarm = malloc(sizeof(buzzer_params_t));
+/**
+ * @brief
+ * @param interval ms
+ * @param percentage 0-100
+ */
+buzzer_alarm_t* buzzer_alarm(uint64_t interval, uint8_t percentage) {
+  buzzer_alarm_t* alarm = malloc(sizeof(buzzer_alarm_t));
   alarm->percentage = percentage;
-  alarm->interval_time = interval_time_ms;
-  xTaskCreate(&alarm_task, "alarm_task", 2048, alarm, 10, &xtask_id);
-  alarm->xtask_id = xtask_id;
+  alarm->interval = interval;
+  alarm->task_handle = malloc(sizeof(TaskHandle_t));
+  xTaskCreate(&alarm_task, "alarm_task", 2048, alarm, 10, alarm->task_handle);
   return alarm;
 }
 
-esp_err_t buzzer_close(buzzer_params_t* alarm) {
-  vTaskDelete(alarm->xtask_id);
+esp_err_t buzzer_close(buzzer_alarm_t* alarm) {
+  vTaskDelete(*alarm->task_handle);
   free(alarm);
   return pwm_stop(LEDC_CHANNEL_7);
 }
--- include/buzzer.h
+++ include/buzzer.h
@@ -4,11 +4,11 @@
 #include "freertos/task.h"
 typedef struct {
   uint8_t percentage;
-  uint64_t interval_time;
-  TaskHandle_t xtask_id;
-} buzzer_params_t;
+  uint64_t interval;
+  TaskHandle_t* task_handle;
+} buzzer_alarm_t;
 
-esp_err_t buzzer_config(int buzzer_pin);
-esp_err_t buzzer_once(uint64_t time_us, uint8_t percentage);
-buzzer_params_t* buzzer_alarm(uint64_t interval_time_ms, uint8_t percentage);
-esp_err_t buzzer_close(buzzer_params_t* alarm);
+esp_err_t buzzer_init(int buzzer_pin);
+esp_err_t buzzer_beep(uint64_t time_us, uint8_t percentage);
+buzzer_alarm_t* buzzer_alarm(uint64_t interval_time_ms, uint8_t percentage);
+esp_err_t buzzer_close(buzzer_alarm_t* alarm);
