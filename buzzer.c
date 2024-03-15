58ff47856965bacaf12141653a4301eb7a502566
myfreax <web@myfreax.com>
2024-03-15 10:39:07 +0800

    refactor: buzzer alarm can be turned off automatically

--- buzzer.c
+++ buzzer.c
@@ -4,6 +4,7 @@
 #include "esp_err.h"
 #include "esp_log.h"
 #include "pwm.h"
+#include "time.h"
 #include "timer.h"
 static const char* TAG = "BUZZER";
 
@@ -20,8 +21,12 @@ esp_err_t buzzer_init(int buzzer_pin) {
   return pwm_config(LEDC_TIMER_3, LEDC_TIMER_10_BIT, 2700, LEDC_AUTO_CLK,
                     LEDC_CHANNEL_7, buzzer_pin);
 }
-
-esp_err_t buzzer_beep(uint64_t time_us, uint8_t percentage) {
+/**
+ * @brief
+ * @param duration buzzer beep duration uint: us
+ * @param percentage 0-100 sound volume
+ */
+esp_err_t buzzer_beep(uint64_t duration, uint8_t percentage) {
   esp_err_t error = pwm_set_duty(LEDC_CHANNEL_7, LEDC_TIMER_10_BIT, percentage);
   if (error != ESP_OK) {
     return error;
@@ -30,37 +35,59 @@ esp_err_t buzzer_beep(uint64_t time_us, uint8_t percentage) {
   static esp_timer_handle_t handle;
 
   ESP_ERROR_CHECK(
-      set_timeout("buzzer_beep", callback, &handle, &handle, time_us));
+      set_timeout("buzzer_beep", callback, &handle, &handle, duration));
   return ESP_OK;
 }
 
 static void alarm_task(void* arg) {
-  buzzer_alarm_t* alarm = arg;
+  buzzer_alarm_t* alarm = (buzzer_alarm_t*)arg;
+  int64_t start = time_current_us();
+
   while (1) {
-    ESP_ERROR_CHECK(
-        pwm_set_duty(LEDC_CHANNEL_7, LEDC_TIMER_10_BIT, alarm->percentage));
-    vTaskDelay(alarm->interval / portTICK_PERIOD_MS);
-    ESP_ERROR_CHECK(pwm_stop(LEDC_CHANNEL_7));
+    if (alarm->timeout != 0 &&
+        time_current_us() - start > alarm->timeout * 1000 * 1000) {
+      if (alarm->callback != NULL) {
+        alarm->callback(alarm->arg);
+      }
+      vTaskDelete(NULL);
+    }
+    pwm_set_duty(LEDC_CHANNEL_7, LEDC_TIMER_10_BIT, alarm->percentage);
+    vTaskDelay(alarm->duration / portTICK_PERIOD_MS);
+    pwm_stop(LEDC_CHANNEL_7);
     vTaskDelay(alarm->interval / portTICK_PERIOD_MS);
   }
 }
 
 /**
  * @brief
- * @param interval ms
- * @param percentage 0-100
+ * @param duration buzzer beep duration uint: ms
+ * @param interval time(ms) interval between beeps
+ * @param percentage 0-100 sound volume
+ * @param timeout After how many milliseconds to close. If it is 0, it will
+ * continue until you manually close it.
+ * @param arg
+ * @note When using automatic shutdown, you need to free buzzer_alarm_t
  */
-buzzer_alarm_t* buzzer_alarm(uint64_t interval, uint8_t percentage) {
+buzzer_alarm_t* buzzer_alarm(uint64_t duration, uint64_t interval,
+                             uint8_t percentage, uint64_t timeout, void* arg,
+                             buzzer_alarm_callback callback) {
   buzzer_alarm_t* alarm = malloc(sizeof(buzzer_alarm_t));
+  alarm->duration = duration;
   alarm->percentage = percentage;
   alarm->interval = interval;
-  alarm->task_handle = malloc(sizeof(TaskHandle_t));
-  xTaskCreate(&alarm_task, "alarm_task", 2048, alarm, 10, alarm->task_handle);
+  alarm->arg = arg;
+  alarm->callback = callback;
+  alarm->task_handle = NULL;
+  alarm->timeout = timeout;
+  xTaskCreate(alarm_task, "alarm_task", 2048, alarm, 10, &alarm->task_handle);
   return alarm;
 }
 
 esp_err_t buzzer_close(buzzer_alarm_t* alarm) {
-  vTaskDelete(*alarm->task_handle);
+  eTaskState state = eTaskGetState(alarm->task_handle);
+  if (state < 4) {
+    vTaskDelete(alarm->task_handle);
+  }
   free(alarm);
   return pwm_stop(LEDC_CHANNEL_7);
 }
--- include/buzzer.h
+++ include/buzzer.h
@@ -2,13 +2,21 @@
 #include "esp_err.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+
+typedef void (*buzzer_alarm_callback)(void* arg);
 typedef struct {
+  uint64_t duration;
   uint8_t percentage;
   uint64_t interval;
-  TaskHandle_t* task_handle;
+  TaskHandle_t task_handle;
+  uint64_t timeout;
+  void* arg;
+  buzzer_alarm_callback callback;
 } buzzer_alarm_t;
 
 esp_err_t buzzer_init(int buzzer_pin);
-esp_err_t buzzer_beep(uint64_t time_us, uint8_t percentage);
-buzzer_alarm_t* buzzer_alarm(uint64_t interval_time_ms, uint8_t percentage);
+esp_err_t buzzer_beep(uint64_t duration, uint8_t percentage);
+buzzer_alarm_t* buzzer_alarm(uint64_t duration, uint64_t interval,
+                             uint8_t percentage, uint64_t timeout, void* arg,
+                             buzzer_alarm_callback callback);
 esp_err_t buzzer_close(buzzer_alarm_t* alarm);
